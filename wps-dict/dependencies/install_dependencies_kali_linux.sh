b54aefdf38ebcdfae85a54c130e8fbebda1e4518
luskaner <luskaner@gmail.com>
2016-08-25 15:38:40 +0200

    Remove unneeded dependencies folder

--- wps-dict/dependencies/install_dependencies_kali_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get install python3-colorama
-apt-get install python3-netaddr
-apt-get install python3-pyquery
--- wps-dict/dependencies/install_dependencies_wifislax_4.12.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo pip3 install netaddr
-sudo pip3 install pyquery
-sudo pip3 install colorama
